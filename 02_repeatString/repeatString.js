e68779b0d5d915a662c238697b817fa809a9bb18
Christian <hacklypselon@gmail.com>
2025-02-12 18:31:21 +0100

    Finished the 02 Task of JavaScript Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) return "ERROR";
+  let finalString = "";
+  for (let i = 0; i < num; i++) {
+    finalString += str;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
