1a56f44a1783eafaf59fc3726cc8a295c5f558b9
Christian <hacklypselon@gmail.com>
2025-02-12 22:58:54 +0100

    Finished the 04 Task of JavaScript Exercises, my solution sucks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArgs) {
+  const newArr = [];
+  for (const arg of theArgs) {
+    const indices = [];
+    let idx = arr.indexOf(arg);
+    while (idx !== -1) {
+      indices.push(idx);
+      idx = arr.indexOf(arg, idx + 1);
+    }
+    indices.reverse();
+    for (const index of indices) {
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
