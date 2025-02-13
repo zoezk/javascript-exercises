69740e60e1a3a1862738bdac6cf4b39def5f93f2
Christian <hacklypselon@gmail.com>
2025-02-13 02:22:31 +0100

    Finished the 06 Task of JavaScript Exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 === 0 && year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  if (year % 4 === 0) return true;
+  return false;
 };
 
 // Do not edit below this line
