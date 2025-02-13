a692e66492f0edb6d093ca89d260c0bba1e0783a
Christian <hacklypselon@gmail.com>
2025-02-13 02:03:22 +0100

    Finished the 05 Task of JavaScript Exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  if (
+    numOne < 0 ||
+    numTwo < 0 ||
+    !Number.isInteger(numOne) ||
+    !Number.isInteger(numTwo)
+  ) {
+    return "ERROR";
+  }
 
+  const [start, end] = numOne < numTwo ? [numOne, numTwo] : [numTwo, numOne];
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
