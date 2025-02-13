5a2617b48649f1438f07c8fec621a516717ea948
Christian <hacklypselon@gmail.com>
2025-02-13 11:22:15 +0100

    Finished the 07 Task of JavaScript Exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const result = (fahrenheit - 32) * (5 / 9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const result = (9 / 5) * celsius + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
