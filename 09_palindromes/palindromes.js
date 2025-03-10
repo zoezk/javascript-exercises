2e2579590e41aacbe6ef797a3ab286d4a8cb3bca
Christian <hacklypselon@gmail.com>
2025-03-10 19:16:52 +0100

    Finished the 09 Task of JavaScript Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  console.log(
+    string.replace(/[, !.]/g, "").toLowerCase(),
+    string
+      .replace(/[, !.]/g, "")
+      .toLowerCase()
+      .split("")
+      .reverse()
+      .join("")
+  );
+  return (
+    string.replace(/[, !.]/g, "").toLowerCase() ===
+    string
+      .replace(/[, !.]/g, "")
+      .toLowerCase()
+      .split("")
+      .reverse()
+      .join("")
+  );
 };
 
 // Do not edit below this line
