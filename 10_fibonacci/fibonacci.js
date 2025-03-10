f2983ac7676b3fa771dc2929a9e332d89da1f45f
Christian <hacklypselon@gmail.com>
2025-03-10 21:45:16 +0100

    Finished the 10 Task of JavaScript Exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (+num === 0) return 0;
+  if (num < 0) return "OOPS";
+  const arr = [1, 1];
+  for (let i = 2; i < +num; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[num - 1];
 };
 
 // Do not edit below this line
