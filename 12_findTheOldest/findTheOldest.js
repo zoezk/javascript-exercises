f162cbc569ecb8a7b5f622f24a6c9a186246b2f2
Christian <hacklypselon@gmail.com>
2025-03-10 22:14:08 +0100

    Finished the 12 Task of JavaScript Exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let oldest = arr[0];
+  arr.forEach((item, index) => {
+    if (!item.yearOfDeath) item.yearOfDeath = new Date().getFullYear();
+    if (
+      item.yearOfDeath - item.yearOfBirth >=
+      oldest.yearOfDeath - oldest.yearOfBirth
+    )
+      oldest = item;
+  });
+  return oldest;
 };
 
 // Do not edit below this line
